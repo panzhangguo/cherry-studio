613e80b58865038b4d22f5fc00b1bd8ca19efb06
小马驹玩溜溜球 <943695464@qq.com>
2025-03-25 08:56:51 +0800

    chore: 添加自定义环境变量

--- src/renderer/src/config/env.ts
+++ src/renderer/src/config/env.ts
@@ -1,5 +1,26 @@
 export { default as UserAvatar } from '@renderer/assets/images/avatar.png'
 export { default as AppLogo } from '@renderer/assets/images/logo.png'
 
-export const APP_NAME = 'Cherry Studio'
+export const APP_NAME = 'Winload Studio' // 修改环境文件APP_NAME
 export const isLocalAi = false
+
+// pfee 添加自定义环境变量
+export const isPfeeShow = false
+
+export const EXPAND_CONFIG = {
+  API_URL: import.meta.env.DEV ? 'http://10.16.2.185:3000/api' : 'http://111.231.8.34:3000/api',
+  UPLOAD_URL: import.meta.env.DEV ? 'http://10.16.2.185:3000/api' : 'http://111.231.8.34:3000/api',
+  urlPrefix: '',
+  AuthApi: {
+    //获取短信验证码的接口
+    getCaptcha: '/sys/sms',
+    //注册接口
+    register: '/sys/user/register',
+    // 退出
+    logout: '/user/logout',
+    // 登录
+    login: '/user/login?turnstile=',
+    // 用户模型
+    models: '/user/models'
+  }
+}
